830ed40d010e2bfa039f12ae92c46a4b77cf2db8
erlebach <gordon.erlebach@gmail.com>
2018-09-25 07:17:57 -0400

    Added jumping, using jump trigger in Animator, 4 more transitions between idle, walking, jumping, and using AddForce to counter gravity.
    For now, just used a force of +20 in the vertical direction. I really should control the force from the inspector.

--- animation_tutorial/Assets/Class 2018/player_anim_script.cs
+++ animation_tutorial/Assets/Class 2018/player_anim_script.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class player_anim_script : MonoBehaviour {
 	public float speed = 10;
+	public Rigidbody2D rb;
 	Animator my_animator;
 
 	// Use this for initialization
 	void Start () {
 		my_animator = GetComponent<Animator>();
+		rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float x = Input.GetAxis("Horizontal");
 		float y = Input.GetAxis("Vertical");
-		print(x);
 		transform.position += x * speed * Time.deltaTime * (new Vector3(1f,0f,0f));
 
-		if (x > 0) {
+		if (Input.GetKey(KeyCode.Space)) {
+			my_animator.SetTrigger("IsJumping");
+			rb.AddForce(new Vector2(0f, 20f));
+			print("jump");
+		} else if (x > 0) {
 			my_animator.SetTrigger("isWalking");
 			transform.localScale = new Vector3(1f,1f,1f);
 		} else if (x < 0) {

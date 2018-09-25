ea0ead9f46e79f5660cdefc5721863d0fb7f83cf
erlebach <gordon.erlebach@gmail.com>
2018-09-25 12:16:00 -0400

    Start writing player script.
    Got to the point of input Horizontal and accessing a trigger.

---
+++ animation_tutorial/Assets/player_sprite_script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_sprite_script : MonoBehaviour {
+	public float speed = 1;
+	public float jump_speed;
+	private Rigidbody2D rb;
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis("Horizontal");
+
+		if (x < 0) {
+			animator.SetTrigger("isWalking");
+		} else {
+
+		}
+	}
+}

c068e6d534a85f702092f600d6a24642b432b0d0
erlebach <gordon.erlebach@gmail.com>
2018-09-25 06:18:52 -0400

    Created two platforms with idle and walking animatoins. Two transitions in animator.
    This almost cover animation video 1 created by Michael Conry in 2017.

---
+++ animation_tutorial/Assets/Class 2018/player_anim_script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_anim_script : MonoBehaviour {
+	public float speed = 10;
+	Animator my_animator;
+
+	// Use this for initialization
+	void Start () {
+		my_animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+		print(x);
+		transform.position += x * speed * Time.deltaTime * (new Vector3(1f,0f,0f));
+
+		if (x > 0) {
+			my_animator.SetTrigger("isWalking");
+			transform.localScale = new Vector3(1f,1f,1f);
+		} else if (x < 0) {
+			my_animator.SetTrigger("isWalking");
+			transform.localScale = new Vector3(-1f,1f,1f);
+		} else {
+			my_animator.SetTrigger("isIdle");
+		}
+	}
+}

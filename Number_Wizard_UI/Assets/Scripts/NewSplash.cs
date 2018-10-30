a36d9c80690f9bfa1990471ee201f0d8426ed415
erlebach <gordon.erlebach@gmail.com>
2018-10-30 07:50:15 -0400

    No real change.

---
+++ Number_Wizard_UI/Assets/Scripts/NewSplash.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewSplash : MonoBehaviour {
+	public int how_long;
+	public LevelManager level_manager;
+
+
+	// Use this for initialization
+	void Start () {
+		// First argument must be a method in this class
+		Invoke("ChangeScene", how_long);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ChangeScene() {
+		level_manager.AutoLoad();
+	}
+}

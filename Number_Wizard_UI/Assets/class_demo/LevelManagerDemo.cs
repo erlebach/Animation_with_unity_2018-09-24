82b60390b870d6446615a0d2fb4ba884175927fc
erlebach <gordon.erlebach@gmail.com>
2018-10-30 12:17:43 -0400

    Class demo of buttons and splash
    Created a splash screen, which changed to start menu after a specified number of seconds (in the inspector)

---
+++ Number_Wizard_UI/Assets/class_demo/LevelManagerDemo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelManagerDemo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void LoadLevel(string name) {
+		print("entered LoadLevel " + name);
+		SceneManager.LoadScene(name);
+
+	}
+
+	public void LoadNextLevel() {
+	}
+}
---
+++ Number_Wizard_UI/Assets/quitSplash.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quitSplash : MonoBehaviour {
+
+	public LevelManagerDemo level_manager;
+	public int start_after = 10;
+
+	// Use this for initialization
+	void Start () {
+		Invoke("LoadLevel", start_after);
+	}
+	
+	public void LoadLevel() {
+		print("LoadLevel from splash");
+		level_manager.LoadLevel("StartMenu");
+	}
+}
